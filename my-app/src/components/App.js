1588aceb69b9da71383b1ce85cb74f06c534fa7d
Cindy Qian <cindyqian.yushi@gmail.com>
2022-11-16 13:54:23 -0800

    fixed a few import links

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import HomePage from './components/HomePage';
-import ComparisonPage from './components/ComparisonPage';
-import ProfilePage from './components/ProfilePage';
+import '../index.css';
+import {HomePage} from './HomePage.js';
+import {ComparisonPage} from './ComparisonPage.js';
+import {ProfilePage} from './ProfilePage.js';
 
 export function App(props) {
     const root = ReactDOM.createRoot(document.getElementById('root'));
