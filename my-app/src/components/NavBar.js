26dfaf56e5e5239876faa2be075171e54e0b23a3
Erickuokuo <eric_kuo@outlook.com>
2022-11-16 13:51:58 -0800

    some nav bar stuff

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -1,10 +1,24 @@
 // NAV BAR, ERIC
-
+import { Link, useMatch, useResolvedPath} from "react-router-dom"
 import React from 'react'; //import React library
 
 export function NavBar(props) {
-    return (
-        // placeholder div, delete
-        <div></div>
+    return ( <nav className = "nav" >
+        <Link to = "/" className = "site-title" > Logo </Link> 
+        <ul>
+        <CustomLink to = "/comparison" > Comparison </CustomLink> 
+        <CustomLink to = "/profile" > Profile </CustomLink> 
+        </ul> 
+        </nav> 
     )
 }
+
+function CustomLink({ to, children, ...props}) {
+    const resolvedPath = useResolvedPath(to)
+    const active = useMatch({ path: resolvedPath.pathname, end:true})
+    return ( <li className = {isActive ? "active" : "" } >
+        < Link to = { to } {...props } > { children } </Link> 
+        </li>
+    )
+}
+
