54a1ae3dce2b3127e5db191d55ab1ab8678542f0
Linh Pham <Lpham21@uw.edu>
2022-12-12 19:24:32 -0800

    resolved errors with buttons

--- my-app/src/components/Button.js
+++ my-app/src/components/Button.js
@@ -2,6 +2,6 @@ import React from 'react'; //import React library
 
 export function Button(props) {
     return (
-        <button type="button" className={props.classStyle} onClick={(event) => props.handleClick(event)}>{props.text}</button>
+        <button type="button" className={props.classStyle} onClick={props.handleClick}>{props.text}</button>
     )
 }
